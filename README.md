c142eaceadd609ae407a29bd1baaae64c559e5b5
DevPrana <preritrana7@gmail.com>
2022-11-06 00:20:59 +0530

    Added discord bot

--- README.md
+++ README.md
@@ -10,5 +10,7 @@ It helps a service understand who the user is, in most cases we pass our credent
 When we use request.get/post and specify an auth parameter (tuple consisting of username and password) requests is applying the credentials using HTTP’s Basic access authentication scheme under the hood. Hence we can use the http basic auth scheme ourself to create an authorizations object like `authentication = HTTPBasicAuth(credentials['username'], credentials['password'])`.
 <br><br>
 
-
+#### Zero shot learning
+ZSL moslty refers to very specific type of task that is learning a classifier on 1 set of labels and then evaluating on a different set of labels the model has never seen before. We will be using this in our case as well where for every repository a person has we will generate a list of candidate labels and calculate their probabilites. 
+Any candidate label above the baseline probability will be accpeted as the skill of the person. (our default baseline baseline is: )
 
---
+++ discord_data/test.py
@@ -0,0 +1,61 @@
+
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+client = discord.Client(command_prefix=',', intents=discord.Intents().all())
+
+@client.event
+async def on_ready():
+    for guild in client.guilds:
+        if guild.name==GUILD:
+            break
+    print(
+        f'{client.user} is connected to following guild\n'
+        f'{guild.name}(id: {guild.id})'
+        )
+    
+    members='\n - '.join([member.name for member in guild.members])
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel_send(f'Hi {member.name}, welcome to this Discord server')
+
+@client.event
+async def on_message(message):
+    
+    '''To prevent a situation where the bot reads its own message and cause an infinite recursion as the client cannot diffrentiate between 
+    a bot and a client we ass this poece of code'''
+    if message.author == client.user:
+        return
+
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool, '
+            'no doubt no doubt no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
+    elif message.content=="raiseException":
+        raise discord.ClientException
+    
+    '''Another potential message reading way is to check for a substring inside a message'''
+    if "happy birthday" in message.content.lower():
+        await message.channel.send("Happy birthday my guy!")
+    
+    # This is a good example where we can see the bot will recurisvely call 
+    # happy birthday since the reponse also contains happy birthday
+
+client.run(TOKEN)
